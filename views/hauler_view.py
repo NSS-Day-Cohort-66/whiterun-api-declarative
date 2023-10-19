dfa4251a35b84e4b153344f8ed7cf07f98be08d4
Kevin Quail <kevinq.7365@gmail.com>
2023-10-19 10:45:15 -0500

    hauler POST complete

--- views/hauler_view.py
+++ views/hauler_view.py
@@ -1,8 +1,20 @@
 import json
 from nss_handler import status
-from repository import db_get_single, db_get_all, db_delete, db_update
+from repository import db_get_single, db_get_all, db_delete, db_update, db_create
 
 class HaulerView():
+
+    def add(self, handler, hauler_data):
+        sql = """
+        INSERT INTO 'Hauler' VALUES (null, ?, ?)
+        """
+        number_of_rows_created = db_create(
+            sql,
+            (hauler_data['name'], hauler_data['dock_id']))
+        if number_of_rows_created > 0:
+            return handler.response("", status.HTTP_201_SUCCESS_CREATED.value)
+        else:
+            return handler.response("", status.HTTP_404_CLIENT_ERROR_RESOURCE_NOT_FOUND.value)
 
     def get(self, handler, pk):
         parsed_url = handler.parse_url(handler.path)
